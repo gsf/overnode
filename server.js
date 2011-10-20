904dd2451bf8af3434efe915c58eb8a8b2a1e669
Gabriel Farrell <g@grrawr.com>
2011-10-20 01:08:09 -0400

    Return 404 on missing hostname

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 var fs = require('fs');
 var http = require('http');
+var path = require('path');
 var url = require('url');
 
 var port = process.argv[2] || 9001;
 var appDir = '/var/node/';
 
 var files = fs.readdirSync(appDir);
+// drop files that aren't directories containing "app.js"
 files.filter(function(file) {
   return (fs.statSync(appDir+file).isDirectory() && 
       path.existsSync(appDir+file+'/app.js'));
 });
 
 http.createServer(function(req, res) {
-  var hostname = url.parse(req.url).hostname;
   for (var i=0, l=files.length; i<l; i++) {
-    if (files[i] == hostname) {
-      require(appDir+files[i]+'app').respond(req, res);
-      break;
+    if (files[i] == req.headers.host) {
+      require(appDir+files[i]+'/app').respond(req, res);
+      return;
     }
   }
+  // no hostname match
+  res.writeHead(404, {'Content-Type': 'text/html'});
+  res.end('<h1>404 Not Found</h1>');
 }).listen(port);
 
 console.log('Running on port '+port);
