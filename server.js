52f14d775dc80df9dd0bb1c97089578ec4702340
Gabriel Farrell <g@grrawr.com>
2011-10-20 11:01:33 -0400

    Use apps object instead of files list

--- server.js
+++ server.js
@@ -6,23 +6,22 @@ var url = require('url');
 var port = process.argv[2] || 9001;
 var appDir = '/var/node/';
 
-var files = fs.readdirSync(appDir);
-// drop files that aren't directories containing "app.js"
-files.filter(function(file) {
-  return (fs.statSync(appDir+file).isDirectory() && 
-      path.existsSync(appDir+file+'/app.js'));
-});
+var files = fs.readdirSync(appDir), apps = {};
+for (var i=0, l=files.length; i<l; i++) {
+  // only include directories containing "app.js"
+  if (path.existsSync(appDir+files[i]+'/app.js')) {
+    apps[files[i]] = require(appDir+files[i]+'/app');
+  }
+}
 
 http.createServer(function(req, res) {
-  for (var i=0, l=files.length; i<l; i++) {
-    if (files[i] == req.headers.host) {
-      require(appDir+files[i]+'/app').respond(req, res);
-      return;
-    }
+  var app = apps[req.headers.host];
+  if (app) {
+    app.respond(req, res);
+  } else { // no hostname match
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.end('<h1>404 Not Found</h1>');
   }
-  // no hostname match
-  res.writeHead(404, {'Content-Type': 'text/html'});
-  res.end('<h1>404 Not Found</h1>');
 }).listen(port);
 
 console.log('Running on port '+port);
